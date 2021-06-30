ceec495a2c003d3d23133492aa80941ffaac8542
inclemenstv <inclemenstv@gmail.com>
2021-06-30 16:38:49 +0300

    initial

---
+++ create_config_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source .env
+
+envsubst < job_config.xml.template > job_config.xml
+envsubst < Vagrantfile.template > Vagrantfile
+envsubst < ./groovy_scripts/init_script.groovy.template > ./groovy_scripts/init_script.groovy
+envsubst < credential.xml.template > credential.xml
+
+
+
+echo "Creating ssh keys"
+ssh-keygen -q -N '' -f ./ssh_keys/id_rsa <<<y 2>&1 >/dev/null
